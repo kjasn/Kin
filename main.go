4112dd5c98e06031094d238db1f7a32d546bcb5b
Kjasn <3384148601@qq.com>
2024-02-04 15:45:05 +0800

    test and fix bugs

--- main.go
+++ main.go
@@ -13,28 +13,28 @@ type Engine struct {
 func main() {
 	router := kin.New()
 
-	// router.GET("/", indexHandler)
-	// router.GET("/ping", func(ctx *kin.Context) {
-	// 	ctx.JSON(http.StatusOK, kin.H{
-	// 		"name": "kjasn", 
-	// 		"opt": "test",
-	// 	})
-	// })
-	// router.GET("/hello", func(ctx *kin.Context) {
-	// 	ctx.String(http.StatusOK, "hello %s, the path is %s\n", 
-	// 	ctx.Query("name"), ctx.Path)
-	// })
-
-	router.POST("/", indexHandler)
-	router.POST("/ping", func(ctx *kin.Context) {
+	router.GET("/", indexHandler)
+	router.GET("/ping", func(ctx *kin.Context) {
 		ctx.JSON(http.StatusOK, kin.H{
 			"name": "kjasn", 
 			"opt": "test",
 		})
 	})
-	router.POST("/hello", func(ctx *kin.Context) {
+	router.GET("/hello", func(ctx *kin.Context) {
 		ctx.String(http.StatusOK, "hello %s, the path is %s\n", 
 		ctx.Query("name"), ctx.Path)
+	})
+
+	// parameters using ':'
+	router.GET("/hello/:lang", func(ctx *kin.Context) {
+		ctx.JSON(http.StatusOK, kin.H {"lang" : ctx.Param("lang")})
+	})
+
+	// wildcard '*' match
+	router.GET("/test/*filepath", func(ctx *kin.Context) {
+		ctx.JSON(http.StatusOK, kin.H {
+			"filepath" : ctx.Param("filepath"),
+		})
 	})
 
 	err := router.Run(":80")
